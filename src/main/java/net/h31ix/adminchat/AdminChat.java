2b2d62c9b929508893cd76f89ddd76447d46548b
h31ix <low_gravity@outlook.com>
2013-08-22 09:59:51 -0400

    Super quick sanity cleanup before I pass on development.

--- src/main/java/net/h31ix/adminchat/AdminChat.java
+++ src/main/java/net/h31ix/adminchat/AdminChat.java
@@ -10,14 +10,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Bukkit;
 
 public class AdminChat extends JavaPlugin {
+
     private int msglength;
-    private boolean send;
     private String[ ] message= new String[20];
     private String result = "";
+	
     @Override
     public void onDisable() {
-        // TODO: Place any custom disable code here.
-        System.out.println(this + " is now disabled!");
     }
 
     @Override
@@ -28,74 +27,62 @@ public class AdminChat extends JavaPlugin {
                 Server server = getServer();
                 String safenick = player.getName().toLowerCase().replaceAll("'", "\"");
                 String safenick1 = safenick.replaceAll("§f", "");
-                if (player.hasPermission("adminchat.broadcast")|| player.isOp())
-                {
-                    if (args.length <= 0)
-                {
-                    player.sendMessage(ChatColor.RED + "Usage: /bc <message>");
-                    send = false;
-                }
-                 else {
-                    msglength = args.length;
-                for (int i = 0;i<msglength;i++)
-                {
-                    message[i]= args[i]; 
-                }
-                result = message[0];
-                for (int i=1; i<msglength; i++) {
-                result = result + " " + message[i];
-                }
-                send = true;
-                }
-                if (send == true)
-                {
-                    Bukkit.getServer().broadcastMessage(ChatColor.GREEN + "ALL: "+safenick1+": "+ChatColor.RED+result);
-                }    
-                }else player.sendMessage(ChatColor.RED + "You dont have permission to use that command.");
+                if (player.hasPermission("adminchat.broadcast")|| player.isOp()) {
+                    if (args.length <= 0) {
+						player.sendMessage(ChatColor.RED + "Usage: /bc <message>");
+                    } else {
+						msglength = args.length;
+						
+						for (int i = 0;i<msglength;i++) {
+							message[i]= args[i]; 
+						}
+						
+						result = message[0];
+						for (int i=1; i<msglength; i++) {
+							result = result + " " + message[i];
+						}
+						Bukkit.getServer().broadcastMessage(ChatColor.GREEN + "ALL: "+safenick1+": "+ChatColor.RED+result);
+					}    
+                } else {
+					player.sendMessage(ChatColor.RED + "You don't have permission to use that command.");
+				}
                 return true; 
-            }
-           }); 
-            getCommand("ac").setExecutor(new CommandExecutor() {
-            public boolean onCommand(CommandSender cs, Command cmnd, String alias, String[] args) {
-                Player player = (Player)cs;
-                Server server = getServer();
-                String safenick = player.getName().toLowerCase().replaceAll("'", "\"");
-                String safenick1 = safenick.replaceAll("§f", "");
-                if (player.hasPermission("adminchat.chat")|| player.isOp())
-                {
-                if (args.length <= 0)
-                {
-                    player.sendMessage(ChatColor.RED + "Usage: /ac <message>");
-                    send = false;
-                }
-                else {
-                    msglength = args.length;
-                for (int i = 0;i<msglength;i++)
-                {
-                    message[i]= args[i]; 
-                }
-                result = message[0];
-                for (int i=1; i<msglength; i++) {
-                result = result + " " + message[i];
-                }
-                send = true;
-                }
-                if (send == true)
-                {
-                Player[] online = server.getOnlinePlayers();
-                for (int o=0;o<online.length;o++)
-                {
-                    Player name = online[o];
-                if (name.hasPermission("adminchat.read") || name.isOp())
-                {
-                  name.sendMessage(ChatColor.GREEN + "ADMIN: "+safenick1+": "+ChatColor.RED+result);  
-                }
-                }
-                }
-                } else player.sendMessage(ChatColor.RED + "You dont have permission to use that command.");
-               return true; 
-            }
-           }); 
-        System.out.println(this + " is now enabled!");
+			}
+        }); 
+		getCommand("ac").setExecutor(new CommandExecutor() {
+			public boolean onCommand(CommandSender cs, Command cmnd, String alias, String[] args) {
+				Player player = (Player)cs;
+				Server server = getServer();
+				String safenick = player.getName().toLowerCase().replaceAll("'", "\"");
+				String safenick1 = safenick.replaceAll("§f", "");
+				if (player.hasPermission("adminchat.chat")|| player.isOp()) {
+					if (args.length <= 0) {
+						player.sendMessage(ChatColor.RED + "Usage: /ac <message>");
+					} else {
+						msglength = args.length;
+
+						for (int i = 0;i<msglength;i++) {
+							message[i]= args[i]; 
+						}
+
+						result = message[0];
+						for (int i=1; i<msglength; i++) {
+							result = result + " " + message[i];
+						}
+						
+						Player[] online = server.getOnlinePlayers();
+						for (int o=0;o<online.length;o++){
+							Player name = online[o];
+							if (name.hasPermission("adminchat.read") || name.isOp()) {
+								name.sendMessage(ChatColor.GREEN + "ADMIN: "+safenick1+": "+ChatColor.RED+result);  
+							}
+						}							
+					}
+				} else { 
+					player.sendMessage(ChatColor.RED + "You don't have permission to use that command.");
+				}
+				return true; 
+			}
+        }); 
     }
 }
